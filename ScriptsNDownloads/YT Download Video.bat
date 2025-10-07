4053be8bbfa108e9e12452dd5e0c346c69c137e0
Foofiewastaken <Foofie.arzeq+git@protonmail.com>
2025-10-07 11:08:25 +0200

    Delete ScriptsNDownloads directory

--- ScriptsNDownloads/YT Download Video.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:loop
-set /p url=Paste YouTube URL (or type EXIT to quit) and press Enter:
-if /i "!url!"=="exit" goto end
-yt-dlp -f "bestvideo+bestaudio/best" --merge-output-format mp4 --cookies www.youtube.com_cookies.txt -o "YT vids/%%(title)s.%%(ext)s" %url%
-goto loop
-:end
-echo Exiting...
-pause
--- ScriptsNDownloads/YT Download no sound.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:loop
-set /p url=Paste YouTube URL (or type EXIT to quit) and press Enter: 
-if /i "!url!"=="exit" goto end
-yt-dlp -f "bestvideo/best" --cookies www.youtube.com_cookies.txt -o "YT vids no sound/%%(title)s.%%(ext)s" %url%
-goto loop
-:end
-echo Exiting...
-pause
